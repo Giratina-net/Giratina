dc01a688789e0c07c6f93c4181b8c09e68e13e65
Siwo951 <56748372+Siwo951@users.noreply.github.com>
2022-02-09 23:45:07 +0900

    変数ミスを修正等

--- discordbot.py
+++ discordbot.py
@@ -11,46 +11,40 @@ client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
 # ギラティナのチャンネルのID
-GIRATINA_CHANNNEL_ID = 940610524415144036
+GIRATINA_CHANNEL_ID = 940610524415144036
 
 
 @bot.event
 async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(
-        traceback.TracebackException.from_exception(orig_error).format())
+    orig_error = getattr(error, 'original', error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+
 # 起動時のメッセージの関数
-
-
 async def ready_greet():
-    channel = bot.get_channel(GIRATINA_CHANNNEL_ID)
+    channel = bot.get_channel(GIRATINA_CHANNEL_ID)
     await channel.send('ギラティナ、オォン！')
 
-# 起動時に挨拶をする
 
-
+# Bot起動時に実行される関数
 @bot.event
 async def on_ready():
     await ready_greet()
 
+
 # ピンポン
-
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-# ドナルドの言葉狩り
-# https://qiita.com/sizumita/items/9d44ae7d1ce007391699
-
 
 @bot.event
 async def on_message(message):
-    # 送信者がbotである場合は弾く
+    # 送信者がBotである場合は弾く
     if message.author.bot:
         return
+    # ドナルドの言葉狩り - https://qiita.com/sizumita/items/9d44ae7d1ce007391699
     # メッセージの本文が ドナルド だった場合
     if 'ドナルド' in str(message.content):
         # 送信するメッセージをランダムで決める
@@ -58,48 +52,43 @@ async def on_message(message):
         await message.channel.send('https://tenor.com/view/ronald-mcdonald-insanity-ronald-mcdonald-gif-21974293')
     await bot.process_commands(message)
 
+
 # チーバくんの、なのはな体操
-
-
 @bot.command()
 async def chiibakun(ctx):
-    await ctx.send('https://youtu.be/dC0eie-WQss')
+    await ctx.send('https://www.youtube.com/watch?v=dC0eie-WQss')
+
 
 # かおすちゃんを送信
-
-
 @bot.command()
 async def kaosu(ctx):
     await ctx.send('https://pbs.twimg.com/media/E512yaSVIAQxfNn?format=jpg&name=large')
 
+
 # イキス
-
-
 @bot.command()
 async def inm(ctx):
     await ctx.send('聖バリ「イキスギィイクイク！！！ンアッー！！！マクラがデカすぎる！！！」\n\n'
                    f'{ctx.author.name}「聖なるバリア －ミラーフォース－、淫夢はもうやめてよ！淫夢ごっこは恥ずかしいよ！」\n\n聖バリ「{ctx.author.name}'
                    '、おっ大丈夫か大丈夫か〜？？？バッチェ冷えてるぞ〜淫夢が大好きだってはっきりわかんだね」')
 
+
 # ギラティナの画像を送る
-
-
 @bot.command()
 async def giratina(ctx):
-    await ctx.send('https://images-ext-2.discordapp.net/external/tlYUDsXqoCwJa6TnXCp6V2EnfB9ziojMGuOb_rt1XuU/https/img.gamewith.jp/article/thumbnail/rectangle/36417.png')
-
-# https://qiita.com/sizumita/items/cafd00fe3e114d834ce3
+    await ctx.send('https://img.gamewith.jp/article/thumbnail/rectangle/36417.png')
 
 
+# bokuseku.mp3 流し逃げ - https://qiita.com/sizumita/items/cafd00fe3e114d834ce3
 @bot.command()
 async def bokuseku(ctx):
     if ctx.author.voice is None:
-        await ctx.channel.send("望月くん・・・ボイスチャンネルに来なさい")
+        await ctx.channel.send('望月くん・・・ボイスチャンネルに来なさい')
         return
     # ボイスチャンネルに接続する
     await ctx.author.voice.channel.connect()
     # 音声を再生する
-    ctx.guild.voice_client.play(discord.FFmpegPCMAudio("bokuseku.mp3"))
+    ctx.guild.voice_client.play(discord.FFmpegPCMAudio('bokuseku.mp3'))
     # 音声が再生中か確認する
     while ctx.guild.voice_client.is_playing():
         await sleep(1)
@@ -107,6 +96,7 @@ async def bokuseku(ctx):
     await ctx.guild.voice_client.disconnect()
 
 
+# *.mp3を*.mp4に変換する
 @bot.command()
 async def mp3tomp4(ctx):
     command = "ffmpeg -loop 1 -r 1 -i input.jpg -i bokuseku.mp3 -vcodec libx264 -acodec aac -strict experimental -ab 320k -ac 2 -ar 48000 -pix_fmt yuv420p -shortest -vb 50k -ab 128k output.mp4"
