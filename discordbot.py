076bd5dad676db3589647ca5765039b951b91cd2
neko25 <neko252222@gmail.com>
2022-09-21 02:36:13 +0900

    htalk実装

--- discordbot.py
+++ discordbot.py
@@ -1123,6 +1123,71 @@ async def hiroyuki(ctx, *arg):
         hiroyuki_msg: discord.Message = await ctx.channel.send(embed=embed)
 
 
+#hiroyuki talk
+@bot.command()
+async def htalk(ctx, *arg):
+    if arg:
+        text = arg[0]
+        embed = discord.Embed(colour=0xff00ff, title=f"{text}を生成中です...")
+        hiroyuki_msg: discord.Message = await ctx.channel.send(embed=embed)
+        headers = {
+            'authority': 'tgeedx93af.execute-api.ap-northeast-1.amazonaws.com',
+            'accept': 'application/json, text/plain, */*'
+        }
+        json_data = {
+            'coefont': '19d55439-312d-4a1d-a27b-28f0f31bedc5',
+            'text':  f'{text}',
+        }
+        response = requests.post('https://tgeedx93af.execute-api.ap-northeast-1.amazonaws.com/production/hiroyuki/text2speech', headers=headers, json=json_data)
+        status = response.json()["statusCode"]
+        if status == 200:
+            embed = discord.Embed(colour=0x4db56a, title=f"音声の生成に成功しました")
+            await hiroyuki_msg.edit(embed=embed)
+            key = response.json()["body"]["wav_key"]
+            headers2 = {
+                'authority': 'johwruw0ic.execute-api.ap-northeast-1.amazonaws.com',
+                'accept': 'application/json, text/plain, */*'
+            }
+            json_data2 = {
+            'voice_key': f'{key}',
+            }
+            response2 = requests.post('https://johwruw0ic.execute-api.ap-northeast-1.amazonaws.com/production/hiroyuki_video', headers=headers2, json=json_data2)
+            url = response2.json()["body"]["url"]
+            embed = discord.Embed(colour=0x4db56a, title=f"動画の生成に成功しました")
+            await hiroyuki_msg.edit(embed=embed)
+            response = requests.get(url)
+            file = open("temp.mp4","wb")
+            for chunk in response.iter_content(100000):
+                file.write(chunk)
+            file.close()
+            stream = ffmpeg.input("temp.mp4") 
+            stream = ffmpeg.output(stream, "temp.mp3") 
+            ffmpeg.run(stream)
+            if ctx.author.voice is None:
+                await ctx.channel.send("なんだろう")
+                return
+            # ボイスチャンネルに接続する
+            if ctx.guild.voice_client is None:
+                await ctx.author.voice.channel.connect()
+            # 音声を再生する
+            ctx.guild.voice_client.play(discord.FFmpegPCMAudio("temp.mp3"))
+            # 音声が再生中か確認する
+            while ctx.guild.voice_client.is_playing():
+                await asyncio.sleep(1)
+            # 切断する
+            await ctx.guild.voice_client.disconnect()
+            os.remove('temp.mp4')
+            os.remove('temp.mp3')
+            return
+        else:
+            embed = discord.Embed(colour=0xff0000, title="生成に失敗しました")
+            await hiroyuki_msg.edit(embed=embed)
+            return
+    if not arg:
+        embed = discord.Embed(colour=0xff0000, title=f"文字を入力してください")
+        hiroyuki_msg: discord.Message = await ctx.channel.send(embed=embed)
+
+
 # removebg
 @bot.command()
 async def removebg(ctx):
