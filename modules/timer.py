a2fdd1633037cd69400ea9b93acf8281f613c381
sehatakuro <sehatakuro@gmail.com>
2023-06-24 23:31:22 +0900

    Co-authored-by: naari3 <naari.named@gmail.com> Co-authored-by: neko-san <ne53@users.noreply.github.com>

--- modules/timer.py
+++ modules/timer.py
@@ -2,18 +2,37 @@ import discord
 import asyncio
 import os
 import math
+from datetime import datetime, timedelta
 
 
 async def timer(minutes, seconds, t_msg):
     total_seconds = minutes * 60 + seconds
     embed = discord.Embed(
-        colour=0x5865F2, title=str(minutes) + ":" + str(seconds).zfill(2)
+        colour=0x5865F2, title=f"{minutes}:{str(seconds).zfill(2)}"
     )
     await t_msg.edit(embed=embed)
-    for i in range(0, total_seconds)[::-1]:
-        await asyncio.sleep(1)
-        minutes_remains = math.floor(i / 60)
+    start_time = datetime.now()
+    while True:
+        await asyncio.sleep(0.7)
+        duration = (datetime.now() - start_time).seconds
+        remains = total_seconds - duration
+        if remains < 0:
+            remains = 0
+        remains_minutes = remains // 60 # // は切り捨て除算
+        remains_seconds = remains % 60 # % はあまり
         embed = discord.Embed(
-            colour=0x5865F2, title=str(minutes_remains) + ":" + str(i % 60).zfill(2)
+            colour=0x5865F2, title=f"{remains_minutes}:{str(remains_seconds).zfill(2)}"
         )
         await t_msg.edit(embed=embed)
+        if remains < 0:
+            break
+        
+
+    # for i in range(0, total_seconds)[::-1]:
+    #     await asyncio.sleep(1)
+    #     remains_minutes = i // 60 # // は切り捨て除算
+    #     remains_seconds = i % 60 # % はあまり
+    #     embed = discord.Embed(
+    #         colour=0x5865F2, title=f"{remains_minutes}:{str(remains_seconds).zfill(2)}"
+    #     )
+    #     await t_msg.edit(embed=embed)
